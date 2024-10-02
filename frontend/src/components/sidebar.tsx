bd1bb24ca68873077714edf4e08b285233f28650
LucasCoppola <lucascoppola21@gmail.com>
2024-10-02 13:04:20 -0300

    add scroll area on playlists

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -4,20 +4,22 @@ import SearchInput from "./search";
 import { Button } from "./ui/button";
 import UploadSong from "./upload-song";
 import UserDropdown from "./user-dropdown";
+import { playlists } from "@/lib/consts";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 export default function Sidebar() {
   const isLoggedIn = true;
 
   return (
-    <div className="w-56 p-4 bg-[#121212] flex flex-col space-y-6">
+    <div className="w-56 p-4 bg-[#121212] flex flex-col h-full">
       <div className="space-y-2">
         {isLoggedIn ? <UserDropdown /> : <Login />}
         <SearchInput />
       </div>
 
-      <div className="text-xs">All Tracks</div>
+      <div className="text-xs mt-6">All Tracks</div>
 
-      <div className="flex items-center justify-between dark">
+      <div className="flex items-center justify-between dark mt-6">
         <span className="text-xs font-semibold text-gray-400">Playlists</span>
         <Button variant="ghost" size="icon" className="h-5 w-5">
           <Plus className="h-3 w-3" />
@@ -25,11 +27,20 @@ export default function Sidebar() {
         </Button>
       </div>
 
-      <div className="space-y-2 flex-grow">
-        <div className="text-xs">YouTube</div>
-        <div className="text-xs">SoundCloud</div>
+      <ScrollArea className="flex-grow mt-2">
+        <div className="space-y-2 pb-4">
+          {playlists.map((playlist, i) => (
+            <div key={i} className="text-xs">
+              {playlist.name}
+            </div>
+          ))}
+        </div>
+        <ScrollBar orientation="vertical" />
+      </ScrollArea>
+
+      <div className="mt-4">
+        <UploadSong />
       </div>
-      <UploadSong />
     </div>
   );
 }
---
+++ frontend/src/lib/consts.ts
@@ -0,0 +1,551 @@
+export const playlists = [
+  {
+    name: "Favourites",
+  },
+
+  {
+    name: "Liked",
+  },
+
+  {
+    name: "Library",
+  },
+  {
+    name: "YouTube",
+  },
+  {
+    name: "Favourites",
+  },
+
+  {
+    name: "Liked",
+  },
+
+  {
+    name: "Library",
+  },
+  {
+    name: "YouTube",
+  },
+  {
+    name: "Favourites",
+  },
+
+  {
+    name: "Liked",
+  },
+];
+
+export const tracks = [
+  {
+    title: "EDM Festival Party",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:23",
+  },
+  {
+    title: "Energising Car Game Sports",
+    artist: "OddVision, Infraction",
+    album: "Royalty Free",
+    duration: "1:37",
+  },
+  {
+    title: "Kick Me",
+    artist: "Infraction, Alexi Action, Dedpled",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Little Ghost",
+    artist: "Infraction, Emerel Gray, mini mielik",
+    album: "Royalty Free",
+    duration: "2:15",
+  },
+  {
+    title: "Phonk Cyberpunk",
+    artist: "Infraction & Lazerpunk",
+    album: "Royalty Free",
+    duration: "2:46",
+  },
+  {
+    title: "Satellite",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "1:34",
+  },
+  {
+    title: "Slap Phonk",
+    artist: "Alexi Action & Dedpled",
+    album: "Royalty Free",
+    duration: "1:53",
+  },
+  {
+    title: "Summer Love",
+    artist: "Heikimer",
+    album: "Royalty Free",
+    duration: "2:38",
+  },
+  {
+    title: "Sun Is Shining",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:04",
+  },
+  {
+    title: "Time To Go",
+    artist: "Balynt",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Wanna Feel It",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:05",
+  },
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    id: 16,
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+  {
+    title: "EDM Festival Party",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:23",
+  },
+  {
+    title: "Energising Car Game Sports",
+    artist: "OddVision, Infraction",
+    album: "Royalty Free",
+    duration: "1:37",
+  },
+  {
+    title: "Kick Me",
+    artist: "Infraction, Alexi Action, Dedpled",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Little Ghost",
+    artist: "Infraction, Emerel Gray, mini mielik",
+    album: "Royalty Free",
+    duration: "2:15",
+  },
+  {
+    title: "Phonk Cyberpunk",
+    artist: "Infraction & Lazerpunk",
+    album: "Royalty Free",
+    duration: "2:46",
+  },
+  {
+    title: "Satellite",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "1:34",
+  },
+  {
+    title: "Slap Phonk",
+    artist: "Alexi Action & Dedpled",
+    album: "Royalty Free",
+    duration: "1:53",
+  },
+  {
+    title: "Summer Love",
+    artist: "Heikimer",
+    album: "Royalty Free",
+    duration: "2:38",
+  },
+  {
+    title: "Sun Is Shining",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:04",
+  },
+  {
+    title: "Time To Go",
+    artist: "Balynt",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Wanna Feel It",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:05",
+  },
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+  {
+    title: "EDM Festival Party",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:23",
+  },
+  {
+    title: "Energising Car Game Sports",
+    artist: "OddVision, Infraction",
+    album: "Royalty Free",
+    duration: "1:37",
+  },
+  {
+    title: "Kick Me",
+    artist: "Infraction, Alexi Action, Dedpled",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Little Ghost",
+    artist: "Infraction, Emerel Gray, mini mielik",
+    album: "Royalty Free",
+    duration: "2:15",
+  },
+  {
+    title: "Phonk Cyberpunk",
+    artist: "Infraction & Lazerpunk",
+    album: "Royalty Free",
+    duration: "2:46",
+  },
+  {
+    title: "Satellite",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "1:34",
+  },
+  {
+    title: "Slap Phonk",
+    artist: "Alexi Action & Dedpled",
+    album: "Royalty Free",
+    duration: "1:53",
+  },
+  {
+    title: "Summer Love",
+    artist: "Heikimer",
+    album: "Royalty Free",
+    duration: "2:38",
+  },
+  {
+    title: "Sun Is Shining",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:04",
+  },
+  {
+    title: "Time To Go",
+    artist: "Balynt",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Wanna Feel It",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:05",
+  },
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+  {
+    title: "EDM Festival Party",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:23",
+  },
+  {
+    title: "Energising Car Game Sports",
+    artist: "OddVision, Infraction",
+    album: "Royalty Free",
+    duration: "1:37",
+  },
+  {
+    title: "Kick Me",
+    artist: "Infraction, Alexi Action, Dedpled",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Little Ghost",
+    artist: "Infraction, Emerel Gray, mini mielik",
+    album: "Royalty Free",
+    duration: "2:15",
+  },
+  {
+    title: "Phonk Cyberpunk",
+    artist: "Infraction & Lazerpunk",
+    album: "Royalty Free",
+    duration: "2:46",
+  },
+  {
+    title: "Satellite",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "1:34",
+  },
+  {
+    title: "Slap Phonk",
+    artist: "Alexi Action & Dedpled",
+    album: "Royalty Free",
+    duration: "1:53",
+  },
+  {
+    title: "Summer Love",
+    artist: "Heikimer",
+    album: "Royalty Free",
+    duration: "2:38",
+  },
+  {
+    title: "Sun Is Shining",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:04",
+  },
+  {
+    title: "Time To Go",
+    artist: "Balynt",
+    album: "Royalty Free",
+    duration: "2:18",
+  },
+  {
+    title: "Wanna Feel It",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:05",
+  },
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+
+  {
+    title: "Wanted",
+    artist: "Infraction",
+    album: "Royalty Free",
+    duration: "2:03",
+  },
+];
