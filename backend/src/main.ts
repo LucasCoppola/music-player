2454e4c9ecb200873270bbe235af38f12cbb3fbd
LucasCoppola <lucascoppola21@gmail.com>
2024-10-04 18:49:45 -0300

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import configuration from 'config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api', {
     exclude: [{ path: 'healthz', method: RequestMethod.GET }],
