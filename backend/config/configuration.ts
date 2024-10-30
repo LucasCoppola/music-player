19d8cc32cea0fdec63a90a8807be3374d454e7b1
LucasCoppola <lucascoppola21@gmail.com>
2024-10-30 10:19:15 -0300

    modify db configuration

--- backend/config/configuration.ts
+++ backend/config/configuration.ts
@@ -2,10 +2,7 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 8080,
   environment: process.env.NODE_ENV,
   database: {
-    host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
   },
   jwt: {
     secret: process.env.JWT_SECRET,
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,9 +11,6 @@ import configuration from '../config/configuration';
 import { User } from './entities/user.entity';
 import { Track } from './entities/track.entity';
 import { Playlist } from './entities/playlist.entity';
-import { Favorites } from './entities/favorites.entity';
-import { ListeningHistory } from './entities/listening_history.entity';
-import { TrackPlayCount } from './entities/track_play_count.entity';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { FileModule } from './file/file.module';
@@ -37,21 +34,11 @@ import { FileModule } from './file/file.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      port: 5432,
-      host: configuration().database.host,
-      username: configuration().database.username,
-      password: configuration().database.password,
-      database: configuration().database.name,
-      entities: [
-        User,
-        Track,
-        Playlist,
-        Favorites,
-        ListeningHistory,
-        TrackPlayCount,
-      ],
-      synchronize: configuration().environment !== 'production',
+      url: configuration().database.url,
+      entities: [User, Track, Playlist],
+      synchronize: configuration().environment !== 'prod',
       autoLoadEntities: true,
+      logging: true,
     }),
     AuthModule,
     UsersModule,
